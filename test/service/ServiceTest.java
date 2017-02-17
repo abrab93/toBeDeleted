07dee7d10d7d3b9172cfaabf7b6cec8cca35b50e
pc& <pc&@192.168.8.100>
2017-02-17 08:33:03 +0000

    add service test

---
+++ test/service/ServiceTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pc&
+ */
+public class ServiceTest {
+    
+    public ServiceTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of add method, of class Service.
+     */
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        int a = 0;
+        int b = 0;
+        Service instance = new Service();
+        int expResult = 0;
+        int result = instance.add(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
